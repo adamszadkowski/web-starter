650deda793ce5554fddc28e6763cacd462ed2ff7
Adam Szadkowski <adam@szadkowski.info>
2018-05-05 20:32:25 +0200

    Add automatic injection of jQuery into js files

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,12 +3,16 @@ const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const common = require('./webpack.config.js');
 
 module.exports = merge(common, {
-    plugins: [
-        new OptimizeCssAssetsPlugin({
-            assetNameRegExp: /\.css$/g,
-            cssProcessor: require('cssnano'),
-            cssProcessorOptions: { discardComments: { removeAll: true } },
-            canPrint: true
-        }),
-    ]
+  plugins: [
+    new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: require('cssnano'),
+      cssProcessorOptions: {
+        discardComments: {
+          removeAll: true
+        }
+      },
+      canPrint: true
+    }),
+  ]
 });
